c66539703c24870ad8bfbfd19e5184fec5fe3491
amykis <amykis@mail.ru>
2024-04-18 08:48:11 +0300

    added function add_contact

--- DZ8.py
+++ DZ8.py
@@ -11,7 +11,7 @@ def menu():
         print('Выберите нужный пункт')
         user_choice = input(' 1 - Добавить контакт \n 2 - Найти контакт \n 3 - Изменить контакт \n 4 - Удалить контакт \n 5 - Импортировать данные \n 6 - Экспортировать данные \n 7 - Посмотреть все контакты \n 8 - выйти из приложения \n >>> ')
         if user_choice == '1':
-            print('В разработке')
+            add_contact()
         elif user_choice == '2':
             print('В разработке')
         elif user_choice == '3':
@@ -29,3 +29,20 @@ def menu():
             exit()
         else:
             print('Неправильно выбрана команда! \n')
+
+# Добавление контакта
+def add_contact():
+    data = [
+        input('Введите фамилию: '),
+        input('Введите имя: '),
+        input('Введите отчество: '),
+        input('Введите номер телефона: '),
+    ]
+    line = ' '.join(data)
+    with open(phonebook, 'a', encoding='utf-8') as file:
+        file.write(line + '\n')
+        print(f'Контакт {data[0]} {data[1]} {data[2]} добавлен!') 
+    return
+            
+phonebook = 'phonebook.txt' 
+menu()
