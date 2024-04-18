0790118d3d98835709bebd37a3493d62b28313df
amykis <amykis@mail.ru>
2024-04-18 11:05:33 +0300

    added search functions contacts

--- DZ8.py
+++ DZ8.py
@@ -13,7 +13,8 @@ def menu():
         if user_choice == '1':
             add_contact()
         elif user_choice == '2':
-            print('В разработке')
+            contact_list = contacts(phonebook)
+            find_contact(contact_list)         
         elif user_choice == '3':
             print('В разработке')
         elif user_choice == '4':
@@ -52,6 +53,51 @@ def get_full_contact(phonebook):
     print('\t\t'.join(headers))
     for line in data:
         print('\t\t'.join(line.split(' ')))
+        
+# Формирование списка ключ <-> значение    
+def contacts(phonebook):
+    with open(phonebook, 'r', encoding='utf-8') as phonebook:
+        data = phonebook.readlines()
+    headers = ['Фамилия', 'Имя', 'Отчество', 'Номер телефона']
+    contact_list = []
+    for line in data:
+        line = line.split()
+        contact_list.append(dict(zip(headers, line)))
+    return contact_list
+
+# Поиск контакта по параметрам
+def search_parameters():
+    search = input('Поиск по\n 1 - фамилии \n 2 - имени \n 3 - отчеству \n 4 - номеру телефона \n >>> ')
+    search_value = None
+    if search == '1':
+        search_value = input('Введите фамилию для поиска: ')
+    elif search == '2':
+        search_value = input('Введите имя для поиска: ')
+    elif search == '3':
+        search_value = input('Введите отчество для поиска: ')
+    elif search == '4':
+        search_value = input('Введите номер телефона для поиска: ')
+    return search, search_value
+
+# Поиск контакта в phonebook.txt
+def find_contact(contact_list):
+    search, search_value = search_parameters()    
+    search_value_dict = {'1': 'Фамилия', '2': 'Имя', '3': 'Отчество', '4': 'Номер телефона'}
+    found_contacts = []
+    for contact in contact_list:
+        if contact[search_value_dict[search]] == search_value:
+            found_contacts.append(contact)
+    if len(found_contacts) == 0:
+        print('Контакт не найден!')
+    else:
+        print_contacts(found_contacts)
+
+# Вывод информации по контакту
+def print_contacts(contact_list: list):
+    for contact in contact_list:
+        for key, value in contact.items():
+            print(f'{key}:{value:12}', end='\n')
+        print()
             
 phonebook = 'phonebook.txt' 
 menu()
