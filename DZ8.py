e89352bf3b6ddb8955b3ca9816d6fa735ad31e1f
amykis <amykis@mail.ru>
2024-04-19 16:13:21 +0300

    added function

--- DZ8.py
+++ DZ8.py
@@ -16,7 +16,7 @@ def menu():
             contact_list = contacts(phonebook)
             find_contact(contact_list)         
         elif user_choice == '3':
-            print('В разработке')
+            modification_contacts(phonebook)
         elif user_choice == '4':
             print('В разработке')
         elif user_choice == '5':
@@ -67,19 +67,25 @@ def contacts(phonebook):
 
 # Поиск контакта по параметрам
 def search_parameters():
-    search = input('Поиск по\n 1 - фамилии \n 2 - имени \n 3 - отчеству \n 4 - номеру телефона \n >>> ')
-    search_value = None
-    if search == '1':
-        search_value = input('Введите фамилию для поиска: ')
-    elif search == '2':
-        search_value = input('Введите имя для поиска: ')
-    elif search == '3':
-        search_value = input('Введите отчество для поиска: ')
-    elif search == '4':
-        search_value = input('Введите номер телефона для поиска: ')
-    return search, search_value
+    while True:
+        search = input('Поиск по\n 1 - фамилии \n 2 - имени \n 3 - отчеству \n 4 - номеру телефона \n >>> ')
+        search_value = None
+        if search == '1':
+            search_value = input('Введите фамилию для поиска: ')
+            return search, search_value
+        elif search == '2':
+            search_value = input('Введите имя для поиска: ')
+            return search, search_value
+        elif search == '3':
+            search_value = input('Введите отчество для поиска: ')
+            return search, search_value
+        elif search == '4':
+            search_value = input('Введите номер телефона для поиска: ')
+            return search, search_value
+        else:
+            print('Неправильно выбрана команда! \n')
 
-# Поиск контакта в phonebook.txt
+# Вывод контакта из phonebook.txt
 def find_contact(contact_list):
     search, search_value = search_parameters()    
     search_value_dict = {'1': 'Фамилия', '2': 'Имя', '3': 'Отчество', '4': 'Номер телефона'}
@@ -98,6 +104,46 @@ def print_contacts(contact_list: list):
         for key, value in contact.items():
             print(f'{key}:{value:12}', end='\n')
         print()
+
+# Изменение контакта
+def modification_contacts(phonebook):
+    contact_list = contacts(phonebook)
+    contact_to_change = search_modification_contact(contact_list) 
+    contact_list.remove(contact_to_change)
+    print('Какое поле вы хотите изменить?')
+    field = input('1 - Фамилия\n2 - Имя\n3 - Номер телефона\n>>>')
+    if field == '1':
+        contact_to_change[0] = input('Введите фамилию: ')
+    elif field == '2':
+        contact_to_change[1] = input('Введите имя: ')
+    elif field == '3':
+        contact_to_change[2] = input('Введите номер телефона: ')
+    contact_list.append(contact_to_change)
+    with open(phonebook, 'w', encoding='utf-8') as file:
+        for contact in contact_list:
+            line = ' '.join(contact) + '\n'
+            file.write(line)
             
+# Поиск контакта для исправления
+def search_modification_contact(contact_list: list):
+    search, search_value = search_parameters()
+    list_result = []
+    for contact in contact_list:
+        for values in contact.values():
+            if values == search_value:
+                list_result.append(contact)
+    if len(list_result) == 1:
+        return list_result[0]
+    elif len(list_result) > 1:
+        print('Найдено несколько контактов')
+        for i in range(len(list_result)):
+            print(f'{i + 1} - {list_result[i]}')
+        num_count = int(input('Выберите номер контакта, который нужно изменить/удалить: '))
+        return list_result[num_count - 1]
+    else:
+        print('Контакт не найден')
+        menu(3)
+    
+
 phonebook = 'phonebook.txt' 
 menu()
